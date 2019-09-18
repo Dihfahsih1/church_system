d23f3f5828586299337b5a16bf02f14788d44580
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-09-18 11:01:24 +0300

    i will pick up from there

--- canonapp/views.py
+++ canonapp/views.py
@@ -2128,6 +2128,7 @@ def Enter_Pledges(request):
     else:
         form=PledgesForm()
         return render(request, 'accountantapp/accountant_enter_pledges.html',{'form':form})
+
 #accountant recording main expenses
 def accountant_enter_main_expenses(request):
     if request.method=="POST":
@@ -2140,6 +2141,7 @@ def accountant_enter_main_expenses(request):
         items = MainExpenses.objects.all()
         context = {'items': items, 'form': form, }
         return render(request, 'accountantapp/accountant_enter_main_expenses.html',context)
+
 #accountant recording small expenses that are not catered for in the main expenses
 def accountant_enter_small_expenses(request):
     if request.method == "POST":
